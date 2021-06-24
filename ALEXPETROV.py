62d711cc4ef3ce7b4d964a549720fc0b6b06b8c3
Alexcatprg <85404592+Alexcatprg@users.noreply.github.com>
2021-06-24 19:18:44 +0700

    Add files via upload

---
+++ ALEXPETROV.py
@@ -0,0 +1,11 @@
+from colorama import init
+from colorama import Fore, Back, Style
+
+init()
+
+print(Fore.CYAN)
+print( "ALEXPETROV " + "production" )
+print(Fore.RED)
+print("Представляет.")
+
+input()
